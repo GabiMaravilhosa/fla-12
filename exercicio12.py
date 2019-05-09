11be9900c2147cb416a8adc520003129ff4c6c31
GabiMaravilhosa <gabibernieri11@gmail.com>
2019-05-09 18:17:05 -0300

    n12

---
+++ exercicio12.py
@@ -0,0 +1,9 @@
+n = 0
+pr = 0
+while n < 9:
+    num = int(input("digite um numero: "))
+    if num % 2 == 1 or num  == 2 :
+        pr = pr + 1
+    n += 1
+print("a quantidade de números primos são:", pr)
+        
